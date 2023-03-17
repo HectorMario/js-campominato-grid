fcda344624e0fb90eca090b21ccbe6d1ce6a1b47
HectorMario <hector.mario.peraz@gmail.com>
2023-03-17 17:32:39 +0100

    finitto

--- js/script.js
+++ js/script.js
@@ -5,24 +5,28 @@ const difi = document.getElementById("level")
 
 
 function incre(){
+    pre.innerHTML = "";
     let i = 1 ;
     vro = difi.value;
     while(i <= 100 && vro == "easy"){
         let divi = grid(i);
         console.log(divi);
         pre.append(divi);
+        divi.classList.add("ten")
         i++
     }
     while(i <= 81 && vro == "medium"){
         let divi = grid(i);
         console.log(divi);
         pre.append(divi);
+        divi.classList.add("nine")
         i++
     }
     while(i <= 49 && vro == "hard"){
         let divi = grid(i);
         console.log(divi);
         pre.append(divi);
+        divi.classList.add("seven")
         i++
     }
     
@@ -31,7 +35,16 @@ function grid(i){
     const hect = document.createElement("div");
     hect.classList.add("ms-container");
     hect.innerHTML = `<span>${i}</span>`;
-    console.log(this);
+    hect.addEventListener("click", function(){
+        const bra = parseInt(this.querySelector("span").textContent);
+        if( bra % 2 === 0){
+            this.classList.add("blue")
+            alert(bra)
+        } else {
+            this.classList.add("red")
+            alert(bra)
+        }
+    });
     return hect
 }
 rator.addEventListener("click", incre)
